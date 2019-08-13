d41cf38a280c929fdeaaad03945adcf641c04ee5
Yew <limtheanyew@gmail.com>
2019-08-13 13:56:16 +0800

    a

--- index.js
+++ index.js
@@ -266,7 +266,10 @@ app.put("/students/:id/absent", (req, res) => {
  * Listen to requests on port 3000
  * ===================================
  */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, '0.0.0.0', () =>
+  console.log("~~~ Tuning in to the waves of port " + PORT + " ~~~")
+);
 
 let onClose = function(){
 
